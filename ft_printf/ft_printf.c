239735c654a8a8e417f2209f778d039c64b3d917
Dan Oliver Monells <daoliver@car6s6.42barcelona.com>
2023-08-28 16:17:40 +0200

    printf_test

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,49 +1,82 @@
-#include "ft_printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 15:12:18 by daoliver          #+#    #+#             */
+/*   Updated: 2023/08/28 16:06:38 by daoliver         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-// Función para manejar los argumentos de formato
-int ft_args(const char *s, va_list ac)
+/*#include "ft_printf.h"*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int	print_char(int c)
 {
-    if (*(s + 1) == '%')
-        return (write(1, (const void *)(s + 1), 1)); // Imprimir un solo carácter '%'
-    else if (*(s + 1) == 'c')
-        return (ft_putchar(va_arg(ac, int))); // Imprimir un carácter (char)
-    else if (*(s + 1) == 's')
-        return (ft_putstr(va_arg(ac, char *))); // Imprimir una cadena (string)
-    else if (*(s + 1) == 'i' || (*(s + 1) == 'd'))
-        return (ft_putnbr(va_arg(ac, int))); // Imprimir un número entero (int)
-    else if (*(s + 1) == 'x')
-        return (ft_putnbrbase(va_arg(ac, unsigned int), ("0123456789abcdef"))); // Imprimir un número en base hexadecimal minúscula
-    else if (*(s + 1) == 'X')
-        return (ft_putnbrbase(va_arg(ac, unsigned int), ("0123456789ABCDEF"))); // Imprimir un número en base hexadecimal mayúscula
-    else if (*(s + 1) == 'u')
-        return (ft_nosign(va_arg(ac, unsigned int))); // Imprimir un número entero sin signo (unsigned int)
-    else if (*(s + 1) == 'p')
-        return (ft_putp(va_arg(ac, unsigned long long))); // Imprimir un puntero (dirección de memoria)
-    return (0);
+	return (write(1, &c, 1));
 }
 
-// Función principal para la impresión formateada
-int ft_printf(const char *s, ...)
+int	print_str(char *str)
 {
-    int c;
-    va_list ac; // Declaración de la lista de argumentos variables
+	int	count;
 
-    c = 0;
-    va_start(ac, s); // Iniciar la lista de argumentos variables con el primer argumento (s)
-    while (*s != '\0')
-    {
-        if (*s == '%') // Si se encuentra un '%' indica que hay un formato
-        {
-            c = c + ft_args(s, ac); // Llamar a la función de manejo de argumentos
-            s++;
-            s++;
-        }
-        else
-        {
-            c = c + ft_putchar(*s); // Imprimir el carácter tal cual
-            s++;
-        }
-    }
-    va_end(ac); // Finalizar el uso de la lista de argumentos variables
-    return (c); // Devolver la cantidad total de caracteres impresos
+	count = 0;
+	while (*str != '\0')
+	{
+		print_char((int)*str);
+		++count;
+		++str;
+	}
+	return (count);
+}
+
+/*HEXA MISSING*/
+
+int	print_form(char spec, va_list ap)
+{
+	int	count;
+
+	count = 0;
+	if (spec == 'c')
+		count += print_char(va_arg(ap, int));
+	else if (spec == 's')
+		count += print_str(va_arg(ap, char *));
+	else if (spec == 'd')
+		count += print_digit(va_arg(ap, int), 10);
+	else if (spec == 'x')
+		count += print_digit(va_arg(ap, unsigned int), 16);
+	else
+		count += write(1, &spec, 1);
+	return (count);
+}
+
+int	ft_printf(char const *form, ...)
+{
+	va_start	ap;
+
+	va_start(ap, form);
+	count = 0;
+	while (*form != '\0')
+	{
+		if (*form == '%')
+			count += print_form(*++form, ap);
+		else
+			count += write(1, form, 1);
+		++form;
+	}
+	va_end(ap);
+	return (count);
+}
+
+int	main(void)
+{
+	int	count;
+
+	count = my_printf("Hello %s\n", "JD");
+	my_printf("The chars written are %d\n", count);
 }
---
+++ ft_printf/main.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <wchar.h>
+#include <fcntl.h>
+
+int	main()
+{
+	printf("%x %d\n", -1, -42);
+
+	return 0;
+}
