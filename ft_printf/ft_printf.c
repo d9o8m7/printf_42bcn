3c9018804107ed48a6819e0a2d0d7ecca1079f80
Dan Oliver Monells <daoliver@cbr14s6.42barcelona.com>
2023-09-15 17:01:02 +0200

    commented main

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: daoliver <daoliver@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 15:12:18 by daoliver          #+#    #+#             */
-/*   Updated: 2023/09/12 13:06:43 by daoliver         ###   ########.fr       */
+/*   Updated: 2023/09/15 17:00:25 by daoliver         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,10 +57,10 @@ int	ft_printf(char const *form, ...)
 	return (count);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int	count;
 
 	count = ft_printf("Hello %s\n", "JD");
 	ft_printf("The chars written are %d\n", count);
-}
+}*/
